055699a06952796bab5ab1fd07ed2fb66faf347f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-08-11 14:32:00 +0700

    feat(extensions): Return response for chaining

--- packages/extensions/src/index.ts
+++ packages/extensions/src/index.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ExtensionsRegistry } from '@celosiajs/core'
+import { CelosiaResponse as CelosiaResponseClass, ExtensionsRegistry } from '@celosiajs/core'
 
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace CelosiaJS {
 		interface CelosiaResponse<Body> {
-			sendInternalServerError: () => void
+			sendInternalServerError: () => CelosiaResponseClass
 		}
 	}
 }
 
 ExtensionsRegistry.registerCelosiaResponseExtension('sendInternalServerError', response => {
-	response.status(500).json({
+	return response.status(500).json({
 		errors: {
 			others: ['Internal server error'],
 		},
