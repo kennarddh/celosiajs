8dc0de4d2768131b5a33149f7f3f1d0bf23a0f23
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-09-30 20:07:46 +0700

    feat(example): Add simple post

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,15 +1,18 @@
 import {
+	BaseController,
 	BaseMiddleware,
 	CelosiaInstance,
 	CelosiaRequest,
 	CelosiaResponse,
 	CelosiaRouter,
 	EmptyObject,
+	IControllerRequest,
 	INextFunction,
 } from '@celosiajs/core'
 import { FileUpload } from '@celosiajs/file-upload'
 
 import FileUploadController from 'Controllers/FileUploadController'
+import { z } from 'zod'
 
 const port = 9999
 
@@ -29,7 +32,25 @@ class Middleware1 extends BaseMiddleware {
 	}
 }
 
+class TestController extends BaseController {
+	public async index(
+		_: EmptyObject,
+		request: IControllerRequest<TestController>,
+		response: CelosiaResponse,
+	) {
+		console.log('controller body', request.body)
+		response.status(200).json({ hi: true })
+	}
+
+	public override get body() {
+		return z.object({
+			str: z.string(),
+		})
+	}
+}
+
 router.post('/file-upload', [new FileUpload({})], [new Middleware1()], new FileUploadController())
+router.post('/test', [], new TestController())
 
 instance.useRouters(router)
 
