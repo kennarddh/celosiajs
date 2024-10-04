db44e715c105b181f732909f7c861f799d23bbf8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-10-04 20:42:16 +0700

    feat(example): Setup simple console logger

--- example/src/index.ts
+++ example/src/index.ts
@@ -6,13 +6,28 @@ import {
 	CelosiaResponse,
 	CelosiaRouter,
 	EmptyObject,
+	Globals,
 	IControllerRequest,
 	INextFunction,
 } from '@celosiajs/core'
 import { FileUpload } from '@celosiajs/file-upload'
 
+import winston from 'winston'
+
 import FileUploadController from 'Controllers/FileUploadController'
 import { z } from 'zod'
+
+import { CelosiaFormat } from '../../packages/logging/dist'
+
+const logger = winston.createLogger({
+	transports: [new winston.transports.Console()],
+	format: winston.format.combine(
+		winston.format.ms(),
+		CelosiaFormat({ inspectOptions: { depth: Infinity } }),
+	),
+})
+
+Globals.logger = logger
 
 const port = 9999
 
@@ -22,13 +37,24 @@ const router = new CelosiaRouter({ strict: true })
 
 class Middleware1 extends BaseMiddleware {
 	override index(
-		data: EmptyObject,
-		request: CelosiaRequest,
-		response: CelosiaResponse,
+		_: EmptyObject,
+		__: CelosiaRequest,
+		___: CelosiaResponse,
 		next: INextFunction,
 	): void {
 		console.log('middleware')
 		next()
+	}
+}
+
+class MiddlewareErrorController extends BaseMiddleware {
+	override index(
+		_: EmptyObject,
+		__: CelosiaRequest,
+		___: CelosiaResponse,
+		____: INextFunction,
+	): void {
+		throw new Error('x')
 	}
 }
 
@@ -51,6 +77,7 @@ class TestController extends BaseController {
 
 router.post('/file-upload', [new FileUpload({})], [new Middleware1()], new FileUploadController())
 router.post('/test', [], new TestController())
+router.post('/middleware-error', [new MiddlewareErrorController()], new TestController())
 
 instance.useRouters(router)
 
