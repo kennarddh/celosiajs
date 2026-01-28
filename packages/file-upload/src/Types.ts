5d6f3ed66b15257d79df9d393fc6a188b3cf4f37
kennarddh <79390992+kennarddh@users.noreply.github.com>
2026-01-28 21:54:39 +0700

    fix(file-upload): Config fields not optional

--- packages/file-upload/src/Types.ts
+++ packages/file-upload/src/Types.ts
@@ -48,37 +48,37 @@ export interface FileUploadLimitsOptions {
 	/**
 	 * Max field name size (in bytes). Default: 100.
 	 */
-	fieldNameSize: number
+	fieldNameSize?: number
 
 	/**
 	 * Max field value size (in bytes). Default: 1048576 (1MB).
 	 */
-	fieldSize: number
+	fieldSize?: number
 
 	/**
 	 * Max number of non-file fields. Default: Infinity.
 	 */
-	fields: number
+	fields?: number
 
 	/**
 	 * For multipart forms, the max file size (in bytes). Default: Infinity.
 	 */
-	fileSize: number
+	fileSize?: number
 
 	/**
 	 * For multipart forms, the max number of file fields. Default: Infinity.
 	 */
-	files: number
+	files?: number
 
 	/**
 	 * For multipart forms, the max number of parts (fields + files). Default: Infinity.
 	 */
-	parts: number
+	parts?: number
 
 	/**
 	 * For multipart forms, the max number of header key-value pairs to parse. Default: 2000 (same as node's http module).
 	 */
-	headerPairs: number
+	headerPairs?: number
 }
 
 export interface FileUploadOptions {
